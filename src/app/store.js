0fa6a938f31d7a8fd664271dafde231bfd833de9
Shin <1542shin@gmail.com>
2022-07-26 22:30:42 -0400

    fix mistake

--- src/app/store.js
+++ src/app/store.js
@@ -3,13 +3,13 @@ import searchTermReducer from '../features/searchTerm/searchTermSlice'
 import allRestaurantsReducer from '../features/allRestaurants/allRestaurantsSlice'
 import favoriteRestaurantsReducer from '../features/favoriteRestaurants/favoriteRestaurantsSlice';
 import yelpSearchReducer from '../features/yelpSearch/yelpSearchSlice'
-import counterReducer from './'
+
 export default configureStore({
   reducer: {
     searchTerm: searchTermReducer,
     allRestaurants: allRestaurantsReducer,
     favoriteRestaurants: favoriteRestaurantsReducer,
     yelpSearch: yelpSearchReducer,
-    counter:counterReducer,
+
   },
 });
